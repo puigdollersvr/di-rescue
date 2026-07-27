b87c1be093e98c67d283db683680b3ea7ca3d997
Ricard <puigdollersvr@gmail.com>
2026-07-27 23:06:16 +0200

    fix: revise adaptive DI restoration DSP

--- Source/DspCore.h
+++ Source/DspCore.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <JuceHeader.h>
+#include <juce_dsp/juce_dsp.h>
 
 #include <array>
 
@@ -24,8 +24,10 @@ public:
 
 private:
     void updateEnvelope(float& env, float target, float attack, float release) const noexcept;
-    void resetFilters() noexcept;
+    void resetInstrumentState() noexcept;
+    void applyInstrument(int index) noexcept;
     void buildCoefficients();
+    float processHarmonicResidual(float input) noexcept;
 
     struct FilterCoeffs
     {
@@ -33,13 +35,16 @@ private:
         juce::dsp::IIR::Coefficients<float>::Ptr body;
         juce::dsp::IIR::Coefficients<float>::Ptr presence;
         juce::dsp::IIR::Coefficients<float>::Ptr top;
-        juce::dsp::IIR::Coefficients<float>::Ptr shelf;
+        juce::dsp::IIR::Coefficients<float>::Ptr naturalShelf;
+        juce::dsp::IIR::Coefficients<float>::Ptr auditionShelf;
         juce::dsp::IIR::Coefficients<float>::Ptr harmHp;
         juce::dsp::IIR::Coefficients<float>::Ptr harmLp;
+        juce::dsp::IIR::Coefficients<float>::Ptr harmPostLp;
     };
 
     double sampleRate = 44100.0;
-    int currentInstrument = 0;
+    int currentInstrument = -1;
+    int pendingInstrument = 0;
 
     std::array<FilterCoeffs, 2> filterCoeffs;
 
@@ -47,39 +52,57 @@ private:
     float targetRestoreNorm = 0.0f;
     float bypassMix = 1.0f;
     float targetBypassMix = 1.0f;
+    float instrumentMix = 1.0f;
+    float targetInstrumentMix = 1.0f;
 
     juce::dsp::IIR::Filter<float> safetyFilter;
 
     juce::dsp::IIR::Filter<float> bodyFilter;
     juce::dsp::IIR::Filter<float> presenceFilter;
     juce::dsp::IIR::Filter<float> topFilter;
-    juce::dsp::IIR::Filter<float> shelfFilter;
+    juce::dsp::IIR::Filter<float> naturalShelfFilter;
+    juce::dsp::IIR::Filter<float> auditionShelfFilter;
 
     juce::dsp::IIR::Filter<float> harmonicHp;
     juce::dsp::IIR::Filter<float> harmonicLp;
+    juce::dsp::IIR::Filter<float> harmonicPostLp;
 
-    float bodyEnv = 0.0f, presEnv = 0.0f, topEnv = 0.0f, signalEnv = 0.0f;
+    float bodyEnv = 0.0f;
+    float presEnv = 0.0f;
+    float topEnv = 0.0f;
+    float signalEnv = 0.0f;
     float correction = 0.0f;
 
-    float fastEnv = 0.0f, slowEnv = 0.0f;
+    float fastEnv = 0.0f;
+    float slowEnv = 0.0f;
+    float previousHarmonicInput = 0.0f;
 
-    float restoreCoef = 0.0f, bypassCoef = 0.0f;
-    float maxTransLinear = 0.0f;
-    float bodyAttackCoef = 0.0f, bodyReleaseCoef = 0.0f;
-    float presAttackCoef = 0.0f, presReleaseCoef = 0.0f;
-    float topAttackCoef = 0.0f, topReleaseCoef = 0.0f;
-    float signalAttackCoef = 0.0f, signalReleaseCoef = 0.0f;
-    float spectralAttackCoef = 0.0f, spectralReleaseCoef = 0.0f;
-    float fastAttackCoef = 0.0f, fastReleaseCoef = 0.0f;
-    float slowAttackCoef = 0.0f, slowReleaseCoef = 0.0f;
+    float restoreCoef = 0.0f;
+    float bypassCoef = 0.0f;
+    float instrumentCoef = 0.0f;
+    float bodyAttackCoef = 0.0f;
+    float bodyReleaseCoef = 0.0f;
+    float presAttackCoef = 0.0f;
+    float presReleaseCoef = 0.0f;
+    float topAttackCoef = 0.0f;
+    float topReleaseCoef = 0.0f;
+    float signalAttackCoef = 0.0f;
+    float signalReleaseCoef = 0.0f;
+    float spectralAttackCoef = 0.0f;
+    float spectralReleaseCoef = 0.0f;
+    float fastAttackCoef = 0.0f;
+    float fastReleaseCoef = 0.0f;
+    float slowAttackCoef = 0.0f;
+    float slowReleaseCoef = 0.0f;
 
-    float inputPeak = 0.0f, outputPeak = 0.0f;
+    float inputPeak = 0.0f;
+    float outputPeak = 0.0f;
 
-    static constexpr float maxSpectralDb = 4.5f;
-    static constexpr float maxTransientDb = 2.0f;
-    static constexpr float levelCompDb = 3.0f;
-    static constexpr float branchDrive = 6.0f;
-    static constexpr float harmonicLevel = 0.08f;
+    static constexpr float naturalSpectralDb = 2.5f;
+    static constexpr float auditionSpectralDb = 4.5f;
+    static constexpr float naturalTransientDb = 1.2f;
+    static constexpr float auditionTransientDb = 0.8f;
+    static constexpr float branchDrive = 4.0f;
 };
 
 } // namespace rescue_dsp
